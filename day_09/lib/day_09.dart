44f5ebcab4475a362b502184fc651895ce615b34
Storm-5 <storm-5@Admin-5s-iMac.local>
2023-07-08 11:54:01 +0800

    add: day_09

--- day_09/lib/day_09.dart
+++ day_09/lib/day_09.dart
@@ -1,3 +1,74 @@
-int calculate() {
-  return 6 * 7;
+class Sprite {
+  Sprite(String name) {
+    this.name = name;
+  }
+  String name = 'Sprite';
+  int x = 0;
+  int y = 0;
+  bool isShow = true;
+  int size = 100;
+  int direction = 90;
+
+  void move(int x) {
+    this.x = x;
+  }
+
+  String say() {
+    return this.name;
+  }
+
+  void changeYby(int y) {
+    this.y = y;
+  }
+
+  void pointInDirection(int direction) {
+    this.direction = direction;
+  }
+  void  setSize(int size){
+    this.size=size;
+  }
+  String getDirection() {
+    String side ='left';
+    if (this.direction < 0 && this.direction > 90) {
+      side = "south west";
+    } else if (this.direction < 90 && this.direction > 180) {
+      side= "north west";
+    } else if (this.direction < -179 && this.direction > -90) {
+      side= "north east";
+    } else if (this.direction < -90 && this.direction > 0) {
+      side= "south east";
+    }
+    else {
+      side='west';
+    }
+    return side;
+
+  }
+}
+
+void main() {
+  final Luca = Sprite('Luca'); //constructor without parameter
+  final Hat = Sprite('Hat');
+  final Kia = Sprite('Kia');
+
+  Luca.move(10);
+  Luca.changeYby(30);
+  Hat.changeYby(-40);
+  Hat.move(-30);
+  Kia.move(100);
+  Kia.move(100);
+  Kia.move(-200);
+  Kia.changeYby(30);
+  Kia.changeYby(-20);
+  print(Luca.x);
+  print(Luca.y);
+  print(Hat.y);
+  print(Hat.x);
+  print(Luca.say());
+  print(Kia.say());
+  print(Hat.say());
+  print(Luca.direction = -160);
+  print(Luca.getDirection());
+  Luca.setSize(50);
+  print(Luca.size);
 }
