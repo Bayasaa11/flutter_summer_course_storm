98556b7b0a32ffd11aaacb3ea6d914a26443744e
Storm-5 <storm-5@Admin-5s-iMac.local>
2023-07-27 12:33:16 +0800

    added

---
+++ day_14_flutter/lib/dart_list.dart
@@ -0,0 +1,15 @@
+var cakes=['tort','dumpling','donut', 'pancake','cake', 'biscuit', 4,true,4.5];
+
+List<String> cakeList=[
+  'tort',
+  'dumpling',
+  'donut',
+  'pancake',
+  'cake',
+  'biscuit'
+];
+void main (){
+  print(cakes);
+  print(cakeList);
+  print(cakeList[5]);
+}
