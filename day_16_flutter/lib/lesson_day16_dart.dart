4b6bf3e5fc4dee4ae40fc770172e8255759b2576
Storm-5 <storm-5@Admin-5s-iMac.local>
2023-08-01 11:50:39 +0800

    add_day_16

---
+++ day_16_flutter/lib/lesson_day16_dart.dart
@@ -0,0 +1,18 @@
+
+
+void main(){
+  int age=16;
+  if(age > 13 && age <23){
+    print('Yes I am a teenegar');
+
+  }
+  else
+    {
+      print("No I'm not a teenegar");
+    }
+  // ternary operator
+  age> 13 && age < 23
+  ? print("Yes I'm a teenager")
+  : print("No I'm not a teenager");
+
+}
