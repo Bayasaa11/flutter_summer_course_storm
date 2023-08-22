0fb8e788d6ef54ad5233975b784845767ed632d9
Storm-5 <storm-5@Admin-5s-iMac.local>
2023-08-22 11:30:29 +0800

    add_lesson23

---
+++ lesson_23_flutter/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_23_flutter/screen_layout.dart';
+import 'package:lesson_23_flutter/screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ScreenLayout(),
+  ));
+}
